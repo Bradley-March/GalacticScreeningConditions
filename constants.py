ab3e21e8e4ed8593f337bbeeb19679c9f8aa00d8
Bradley-March <bradley.march@hotmail.com>
2024-08-12 12:54:23 +0100

    Removed unused constants

--- constants.py
+++ constants.py
@@ -14,17 +14,12 @@ kpc, Mpc = pc * 1e3, pc * 1e6
 
 # Solar parameters
 M_sun = 1.9885e+30 # Solar mass 
-logM_sun = np.log10(M_sun)
-L_sun = 3.846e+26 # Solar luminosity 
-Mag_sun = 4.83 # Solar absolute magnitude
 
 # Cosmological parameters
 h = 0.7 # Hubble factor
 H0 = h * 100.0 * 1000.0 / Mpc # Hubble constant
 omega_m = 0.3 # matter density parameter
-omega_L = 1 - omega_m # cosmological constant density parameter
 rho_c = 3.0 * H0**2 / (8.0 * np.pi * G) # critical density
 rho_m = rho_c * omega_m # mean cosmic matter density
-rho_L = rho_c * omega_L # cosmological constant density
 R0 = 3 * omega_m * H0**2 * (1 + 4 * (1 - omega_m) / omega_m) / c**2 # background curvature
 
